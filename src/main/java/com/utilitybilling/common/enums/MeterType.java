167cf14393a3608e7910090143ce1d36875763cd
Polo <nelsonprox92@gmail.com>
2026-06-05 10:22:18 +0200

    Implement meter management module including CRUD operations, status tracking, and customer association

---
+++ src/main/java/com/utilitybilling/common/enums/MeterType.java
@@ -0,0 +1,6 @@
+package com.utilitybilling.common.enums;
+
+public enum MeterType {
+    WATER,
+    ELECTRICITY
+}
--- src/main/java/com/utilitybilling/customer/entity/Customer.java
+++ src/main/java/com/utilitybilling/customer/entity/Customer.java
@@ -2,6 +2,7 @@ package com.utilitybilling.customer.entity;
 
 import com.utilitybilling.common.entity.AuditableEntity;
 import com.utilitybilling.common.enums.AccountStatus;
+import com.utilitybilling.meter.entity.Meter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,7 +10,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,4 +43,7 @@ public class Customer extends AuditableEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AccountStatus status = AccountStatus.ACTIVE;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Meter> meters = new ArrayList<>();
 }
